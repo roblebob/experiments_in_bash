e3b908b87191971731a9cac935db6954e573b8c4
roblebob <robert.koppisch@gmail.com>
2023-12-03 22:19:17 +0100

    corrected wrong solution of 5/6 true_or_false

--- hyperskill/true_or_false.sh
+++ hyperskill/true_or_false.sh
@@ -2,9 +2,17 @@
 
 #  write your code here
 
-echo "Welcome to the True or False Game!"
+WIN_RESPONSES=("Perfect!" "Awesome!" "You are a genius!" "Wow!" "Wonderful!")
 
-while true; do
+login() {
+    curl --silent --output ID_card.txt http://127.0.0.1:8000/download/file.txt
+    username=$(cat ID_card.txt | sed 's/.*"username": *"\{0,1\}\([^,"]*\)"\{0,1\}.*/\1/')
+    password=$(cat ID_card.txt | sed 's/.*"password": *"\{0,1\}\([^,"]*\)"\{0,1\}.*/\1/')
+
+    login_message=$(curl --silent --cookie-jar cookie.txt --user "${username}:${password}" http://127.0.0.1:8000/login)
+}
+
+show_menue() {
     echo
     echo "0. Exit"
     echo "1. Play a game"
@@ -12,6 +20,45 @@ while true; do
     echo "3. Reset scores"
     echo "Enter an option:"
     read -r option
+}
+ 
+RANDOM(){
+    echo 4096
+}
+
+playing_game() {
+    echo "What is your name?"
+    read -r name
+    score=0
+    while true; do
+
+        question_and_answer=$(curl --silent --cookie cookie.txt --user "${username}:${password}" http://127.0.0.1:8000/game)
+        question=$(echo "$question_and_answer" | sed 's/.*"question": *"\{0,1\}\([^,"]*\)"\{0,1\}.*/\1/')
+        answer=$(echo "$question_and_answer" | sed 's/.*"answer": *"\{0,1\}\([^,"]*\)"\{0,1\}.*/\1/')
+
+        echo "$question"
+        echo "True or False?"
+        read -r user_answer
+
+        if [[ "$user_answer" != "$answer" ]]; then
+            echo "Wrong answer, sorry!"
+            echo "${name} you have ${score} correct answer(s)."
+            echo "Your score is $(($score * 10)) points."
+            break
+        fi
+
+        idx=$((RANDOM % ${#WIN_RESPONSES[@]}))
+        echo "${responses[$idx]}"
+        score=$((score + 1))
+    done
+}
+
+
+echo "Welcome to the True or False Game!" 
+login
+while true; do
+
+    show_menue
 
     case "$option" in
         0 )
@@ -19,7 +66,7 @@ while true; do
             exit
             ;;
         1 )
-            echo "Playing game"
+            playing_game
             ;;
         2 )
             echo "Displaying scores"
@@ -31,16 +78,4 @@ while true; do
             echo "Invalid option!"
             ;;
     esac
-
-
-
-
-
-curl --silent --output ID_card.txt --cookie-jar cookie.txt --user "the_username:the_password" http://127.0.0.1:8000/download/file.txt
-message_from_endpoint=$(cat ID_card.txt)
-echo "Login message: $message_from_endpoint"
-
-
-curl --silent --output question_answer.txt --cookie cookie.txt --user "the_username:the_password" http://127.0.0.1:8000/game
-question_and_answer_from_endpoint=$(cat question_answer.txt)
-echo "Response: $question_and_answer_from_endpoint"
+done
