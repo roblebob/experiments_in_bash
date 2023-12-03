01066b9cb6dc2241371ba4555cb73ef9b7a311f9
roblebob <robert.koppisch@gmail.com>
2023-12-03 18:53:52 +0100

    finished 3/6 true_or_false (That is the question)

--- hyperskill/true_or_false.sh
+++ hyperskill/true_or_false.sh
@@ -7,3 +7,8 @@ echo "Welcome to the True or False Game!"
 curl --silent --output ID_card.txt --cookie-jar cookie.txt --user "the_username:the_password" http://127.0.0.1:8000/download/file.txt
 message_from_endpoint=$(cat ID_card.txt)
 echo "Login message: $message_from_endpoint"
+
+
+curl --silent --output question_answer.txt --cookie cookie.txt --user "the_username:the_password" http://127.0.0.1:8000/game
+question_and_answer_from_endpoint=$(cat question_answer.txt)
+echo "Response: $question_and_answer_from_endpoint"
