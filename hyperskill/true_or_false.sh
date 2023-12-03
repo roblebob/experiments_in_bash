fcb99a1201905be26863d94e7cc61eba2381ecbf
roblebob <robert.koppisch@gmail.com>
2023-12-03 18:38:11 +0100

    finished 2/6 true_or_false (Access granted)

--- hyperskill/true_or_false.sh
+++ hyperskill/true_or_false.sh
@@ -4,5 +4,6 @@
 
 echo "Welcome to the True or False Game!"
 
-curl --silent --output ID_card.txt http://127.0.0.1:8000/download/file.txt
-cat ID_card.txt
+curl --silent --output ID_card.txt --cookie-jar cookie.txt --user "the_username:the_password" http://127.0.0.1:8000/download/file.txt
+message_from_endpoint=$(cat ID_card.txt)
+echo "Login message: $message_from_endpoint"
